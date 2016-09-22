aec741d8774a1468d9f83d61f00e1c232e844592
Vance Molhusen <vance.molhusen@webcoretech.com>
2016-09-22 13:10:50 -0500

    Build 1

---
+++ Phase1/CMSC495_TT.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmsc495_tt;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Manoj
+ */
+public class cmsc495_tt extends Application {
+    
+ //   @Override
+    public void start(Stage primaryStage) {    
+        
+  
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+               // 
+               LogInForm logIn = new LogInForm();
+               logIn.setVisible(true); 
+               launch(args);
+        
+    }
+    
+}
